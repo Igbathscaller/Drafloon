126c6c611080613833ba8b294a5367bcc3a205c5
Igbathscaller <igbathscaller@gmail.com>
2025-07-11 23:18:26 -0400

    started using slash commands

--- MainBot.py
+++ MainBot.py
@@ -1,25 +1,41 @@
 import os
 from dotenv import load_dotenv
 import discord
+from discord import app_commands
+from discord.ext import commands
+
+load_dotenv()
+Test_Guild_Id = os.getenv("Test_Guild_Id")
+print(Test_Guild_Id)
+Token = os.getenv("Discord_Token")
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
+
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"Logged in as {client.user}")
+    try:
+        guild = discord.Object(id=Test_Guild_Id)
+        synced = await client.tree.sync(guild=guild)
+        print(f"Synced {len(synced)} command(s) to guild {guild.id}")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
 
-load_dotenv()
-Token = os.getenv("Discord_Token")
+#First slash command 
+# Remove guild in the future
+# it is connected to specific server for faster testing
+
+@client.tree.command(name="hello", description="Say hi to the bot!", guild=discord.Object(id=Test_Guild_Id))
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello, {interaction.user.mention}!")
+
+
 
 client.run(Token)
